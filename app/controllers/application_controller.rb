1328866e51818e5a35788b5347fb434f8651531b
= <siheonl@andrew.cmu.edu>
2018-06-28 10:56:54 -0400

    before running heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
-	before_action :authorize
 	
 	rescue_from CanCan::AccessDenied do |exception|
 		flash[:error] = "Access Denied."

ef237b1f8b9412fa7505c6eb6ed27a253aaa6b3d
= <siheonl@andrew.cmu.edu>
2018-07-05 12:02:53 -0400

    update

--- app/controllers/instructions_controller.rb
+++ app/controllers/instructions_controller.rb
@@ -1,5 +1,14 @@
 class InstructionsController < ApplicationController
 	def index
 	end
+
+	def create
+		redirect_to login_path if start?
+	end
+
+
+	def start?
+		params[:commit] == "Start"
+	end
 	
 end
